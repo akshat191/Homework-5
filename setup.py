a40ef5738b26fcd4b7905de95d704d262d430be3
akshatkhara <61415262+akshat191@users.noreply.github.com>
2022-12-04 19:33:00 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'exercise5',
+    version = '0.1.0',
+    url = '',
+    description = '',
+    packages = find_packages(),
+    install_requires = [
+        # Github Private Repository
+        'Exercise5 @ git+ssh://git@github.com:akshat191/homework5.git#egg=Exercise5-0.1'
+    ]
+)
